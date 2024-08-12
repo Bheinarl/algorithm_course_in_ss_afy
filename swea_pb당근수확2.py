1d1ded59616c4129368045242b537f60c4f3eeb2
Bheinarl <kdytree10@naver.com>
2024-08-12 17:53:05 +0900

    240812_01

--- swea_pb당근수확2.py
+++ swea_pb당근수확2.py
@@ -1,31 +1,47 @@
-def f(n, m, start_index, arr):
-    global counts
+def f(n, m, arr):
 
-    counts += start_index
+    counts = 0
+    now_carrot = 0
 
-    if arr[start_index] > m:
-        arr[start_index] -= m
-        counts += start_index
-        return f(n, m, start_index, arr)
+    for i in range(n):  # i 는 인덱스 번호임. 헷갈리면 안됨. 칸 번호 아님. 이동할 때 +1 해줘야됨. 미안 바로 까먹음 ㅋㅋ;
+        counts += 1  # 앞으로 한 칸 씩
+        while arr[i] >= 0:
+            if now_carrot == 0:  # 남은 당근 없다
+                if arr[i] > m:
+                    counts += 2 * (i+1)  # 왔다 갔다
+                    arr[i] -= m
+                elif arr[i] == m:
+                    counts += 2 * (i+1)  # 왔다 갔다
+                    arr[i] = 0
+                    break  # 이 칸에 당근 없으니깐 다음 칸으로 가야 해
+                else:
+                    now_carrot = arr[i]
+                    arr[i] = 0
+                    break
+            else:  # 당근 남아있다
+                if arr[i] > m - now_carrot:
+                    counts += 2 * (i+1)  # 왔다 갔다
+                    arr[i] -= m - now_carrot
+                    now_carrot = 0  # 수레를 비우자
+                elif arr[i] == m - now_carrot:
+                    counts += 2 * (i+1)  # 왔다 갔다
+                    arr[i] = 0
+                    now_carrot = 0  # 수레를 비우자
+                    break  # 이 칸에 당근 없으니깐 다음 칸으로 가야 해
+                else:
+                    now_carrot += arr[i]
+                    arr[i] = 0
+                    break  # 이 칸에 당근 없으니깐 다음 칸으로 가야 해
+    counts += n
 
-    elif arr[start_index] == m:
-        arr[start_index] -= m
-        counts += start_index
-        return f(n, m, start_index+1, arr)
-
-    else:
-        arr[start_index] = 0
-
-
-    pass
+    return counts
 
 
 T = int(input())
-for TEST_CASE in range(1, T+1):
+for TEST_CASE in range(1, T + 1):
     N, M = map(int, input().split())
     ARR = list(map(int, input().split()))
 
-    counts = 0
-    RESULT = f(N, M, 1, ARR)
+    RESULT = f(N, M, ARR)
 
     print(f'#{TEST_CASE} {RESULT}')
--- swea_pb당근포장하기.py
+++ swea_pb당근포장하기.py
@@ -1,7 +1,57 @@
+def f(n, arr):
+
+    for a in range(0, n - n//2):
+        if arr[a] == arr[a+n//2]:
+            return -1
+
+    carrot_size = [0] * (arr[-1]+1)
+    carrot_max_size = arr[-1]
+    for num in arr:
+        carrot_size[num] += 1
+
+    ans = 1001
+    for i in range(carrot_max_size):  # 3분할 하기 위해서 2중 for문 사용
+        for j in range(i + 1, carrot_max_size + 1):
+            # print(i, j)
+            small_box = 0  # i를 기준으로 앞 부분은 작은 박스
+            for small in range(i):
+                small_box += carrot_size[small]
+            if small_box == 0 or small_box > n//2:  # 만약 용량보다 많거나, 1개도 들지 않았다면 다음 루프로
+                continue
+
+            mid_box = 0
+            for mid in range(i, j):  # i와 j 사이의 부분은 중간 박스
+                mid_box += carrot_size[mid]
+            if mid_box == 0 or mid_box > n//2:
+                continue
+
+            big_box = 0
+            for big in range(j, carrot_max_size + 1):  # j를 기준으로 이후는 큰 박스
+                big_box += carrot_size[big]
+            if big_box == 0 or big_box > n//2:
+                continue
+
+            # print('@@@', [small_box, mid_box, big_box])
+
+            min_d, max_d = 1001, -1  # 최소값, 최대값 측정
+            for box in [small_box, mid_box, big_box]:
+                if min_d > box:
+                    min_d = box
+                if max_d < box:
+                    max_d = box
+            temp_d = max_d - min_d  # 둘의 차이의 최소값을 확인 후 할당
+            if ans >= temp_d:
+                ans = temp_d
+
+    return ans
 
 
 T = int(input())
 for TEST_CASE in range(1, T+1):
     N = int(input())
-    C = list(map(int, input().split()))
+    ARR = list(map(int, input().split()))
+
+    RESULT = f(N, ARR)
+
+    print(f'#{TEST_CASE} {RESULT}')
 
