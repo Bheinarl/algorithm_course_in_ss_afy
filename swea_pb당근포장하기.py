a1b0629e2342b5a298e284d6e40bc2be73abe078
Bheinarl <kdytree10@naver.com>
2024-08-13 01:14:56 +0900

    240812_02

--- swea_pb당근포장하기.py
+++ swea_pb당근포장하기.py
@@ -1,49 +1,24 @@
 def f(n, arr):
 
-    for a in range(0, n - n//2):
-        if arr[a] == arr[a+n//2]:
-            return -1
+    # for k in range(n-n//2):          N = 4, ARR = [1, 1, 2, 2]는 못 거름
+    #     if arr[k] == arr[k+n//2]:
+    #         return -1
+    arr.sort()
+    min_diff = 3000
+    for i in range(n - 2):
+        for j in range(i + 1, n - 1):  # 마지막 상자에 한 개 넣을 꺼 남겨 둬
+            if arr[i] != arr[i + 1] and arr[j] != arr[j + 1]:  # small 과 medium 중복 방지 & medium 과 large 중복 방지
+                small_num = i + 1
+                medium_num = j - i
+                large_num = n - j - 1
+                if small_num <= n // 2 and medium_num <= n // 2 and large_num <= n // 2:  # 박스 다 개수 초과 금지
+                    if min_diff >= max(small_num, medium_num, large_num) - min(small_num, medium_num, large_num):
+                        min_diff = max(small_num, medium_num, large_num) - min(small_num, medium_num, large_num)
 
-    carrot_size = [0] * (arr[-1]+1)
-    carrot_max_size = arr[-1]
-    for num in arr:
-        carrot_size[num] += 1
-
-    ans = 1001
-    for i in range(carrot_max_size):  # 3분할 하기 위해서 2중 for문 사용
-        for j in range(i + 1, carrot_max_size + 1):
-            # print(i, j)
-            small_box = 0  # i를 기준으로 앞 부분은 작은 박스
-            for small in range(i):
-                small_box += carrot_size[small]
-            if small_box == 0 or small_box > n//2:  # 만약 용량보다 많거나, 1개도 들지 않았다면 다음 루프로
-                continue
-
-            mid_box = 0
-            for mid in range(i, j):  # i와 j 사이의 부분은 중간 박스
-                mid_box += carrot_size[mid]
-            if mid_box == 0 or mid_box > n//2:
-                continue
-
-            big_box = 0
-            for big in range(j, carrot_max_size + 1):  # j를 기준으로 이후는 큰 박스
-                big_box += carrot_size[big]
-            if big_box == 0 or big_box > n//2:
-                continue
-
-            # print('@@@', [small_box, mid_box, big_box])
-
-            min_d, max_d = 1001, -1  # 최소값, 최대값 측정
-            for box in [small_box, mid_box, big_box]:
-                if min_d > box:
-                    min_d = box
-                if max_d < box:
-                    max_d = box
-            temp_d = max_d - min_d  # 둘의 차이의 최소값을 확인 후 할당
-            if ans >= temp_d:
-                ans = temp_d
-
-    return ans
+    if min_diff == 3000:
+        return -1
+    else:
+        return min_diff
 
 
 T = int(input())
@@ -54,4 +29,3 @@ for TEST_CASE in range(1, T+1):
     RESULT = f(N, ARR)
 
     print(f'#{TEST_CASE} {RESULT}')
-
