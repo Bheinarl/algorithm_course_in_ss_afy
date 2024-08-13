7e33d366e2d7d51dadaec63a1a18b98f6038ed29
Bheinarl <kdytree10@naver.com>
2024-08-14 00:54:20 +0900

    240813_02

--- day12_swea암호생성기.py
+++ day12_swea암호생성기.py
@@ -2,23 +2,20 @@ def f(arr):
 
     front = 0
     sub_num = 0
-    while arr[front] > sub_num:
+    while arr[front] > sub_num % 5 + 1:  # 빼면 원소가 0이 될 때 (암호가 될 때)
 
-        sub_num = (sub_num) % 5 + 11  # sub_num = 1, 2, 3, 4, 5
+        sub_num = sub_num % 5 + 1  # sub_num = 1, 2, 3, 4, 5
         arr[front] -= sub_num
-        front = (front+1)%8
+        front = (front+1) % 8  # front = 0, 1, 2, 3, 4, 5, 6, 7
 
-    if arr[front] == sub_num:
-        arr[front] - sub_num
-    elif arr[front] <= sub_num:
-        arr[front] = 0
+    arr[front] = 0  # while 문 빠져 나온건 빼면 0이 될 때이다.
 
-    front = (front+1)%8
+    front = (front+1) % 8  # 0인 원소를 맨 뒤로 이동
 
     lst = []
     for _ in range(8):
         lst += [arr[front]]
-        front = (front+1)%8
+        front = (front+1) % 8
     return lst
 
 
@@ -29,7 +26,5 @@ for _ in range(10):
 
     RESULT = f(ARR)
 
-    print(f'#{TEST_CASE}',end=' ')
+    print(f'#{TEST_CASE}', end=' ')
     print(*RESULT)
-
-# 왜 안될까
---
+++ swea_pb1959두개의숫자열.py
@@ -0,0 +1,32 @@
+def f(n, m, arr_a, arr_b):
+
+    max_sum_number = -999999  # 진짜 이렇게 하기 싫은데 max 초기값 설정
+
+    if n <= m:  # B의 길이가 더 길거나 같을 때
+        for i in range(0, m-n+1):  # 움직일 범위 설정
+            sum_number = 0
+            for j in range(n):  # 고정 됐을 때, 계산 수행
+                sum_number += arr_a[j] * arr_b[j+i]  # B가 더 길기 때문에 B의 인덱스를 j + i
+            if max_sum_number <= sum_number:
+                max_sum_number = sum_number
+
+    else:  # A의 길이가 더 길 때
+        for i in range(0, n-m+1):  # 움직일 범위 설정
+            sum_number = 0
+            for j in range(m):  # 고정 됐을 때, 계산 수행
+                sum_number += arr_a[j+i] * arr_b[j]  # A가 더 길기 때문에 A의 인덱스를 j + i
+            if max_sum_number <= sum_number:
+                max_sum_number = sum_number
+
+    return max_sum_number
+
+
+T = int(input())
+for TEST_CASE in range(1, T+1):
+    N, M = map(int, input().split())
+    ARR_A = list(map(int, input().split()))
+    ARR_B = list(map(int, input().split()))
+
+    RESULT = f(N, M, ARR_A, ARR_B)
+
+    print(f'#{TEST_CASE} {RESULT}')
